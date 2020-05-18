04dbc5ee597509a0722226a739281b2c00a91ec2
svedhag <svedhag@kth.se>
2020-05-18 16:24:18 +0200

    Updated picture urls

--- src/components/projects.js
+++ src/components/projects.js
@@ -55,7 +55,7 @@ class Projects extends Component {
                 <div className="projects-grid">
                     {/*Master Thesis*/}
                     <Project 
-                        projectPicture = "url(https://i.ibb.co/Wy02PLT/master.png) center / cover"
+                        projectPicture = "url(https://raw.githubusercontent.com/swedhag/portfolio2/master/public/master.png) center / cover"
                         projectTitle = "Master Thesis"
                         projectDescription = "For my Master thesis, I was tasked with quanitatively investigating the visual performance of 2D Treemap animations. These animations were to be optimized, in regard to 2D Treemap stability, by translating the results of a 1D Treemap rendering algorithm to the 2D case. The translation required programming in both C++ and Java. In the end, the translation was succesful and the results showed clear evidence for 2D Treemap animation, generated with the Slice-and-Dice layout, being stabilized by the 1D rendering algorithm. The paper will be published here once it has passed through the grading administration and has been posted online. "
                         gitHub= ""
@@ -67,7 +67,7 @@ class Projects extends Component {
 
                     {/*Bachelor Thesis*/}
                     <Project 
-                        projectPicture = "url(https://i.ibb.co/tz1hkJ5/bachelor.png) center / cover"
+                        projectPicture = "url(https://raw.githubusercontent.com/swedhag/portfolio2/master/public/bachelor.png) center / cover"
                         projectTitle = "Bachelor Thesis"
                         projectDescription = "Paired with my writing partner, our Bachelor thesis aimed to investigate the effects of positively reinforcing sounds in online gambling. In particular, we sought to determine whether the risk taking of online gamblers was affected by such sound effects. Consequently, we set about developing a Black Jack game with pre-programmed hands and outcomes for every user. 41 users were rounded up to play the game and for each one of these, we logged the size of each bet and the time spent on each round. The surprising findings of our experiment can be accessed by reading the paper attached below. The Black Jack game was coded in Java."
                         gitHub= ""
@@ -79,7 +79,7 @@ class Projects extends Component {
 
                     {/*TouARs*/}
                     <Project 
-                        projectPicture = "url(https://i.ibb.co/JBPbwXt/TouARs.jpg) center / cover"
+                        projectPicture = "url(https://raw.githubusercontent.com/swedhag/portfolio2/master/public/TouARs.jpg) center / cover"
                         projectTitle = "TouARs - An AR Tour Guide"
                         projectDescription = "For this project, my team and I were interested in developing an Augmented Reality (AR) service. Due to the fact that some of us were history buffs, it was decided we should develop a virtual and historical tour guide. Along with the development of the AR service, we set out with the task of investigating the opportunity for services like this one to exist in the smart tourism industry of the near future. A qualitative study was consequently conducted, during which users got to interact with a small mock-up city tour, set up in one of the computer labs of KTH. The service was developed in Unity, along with the Vuforia AR SDK, and programmed in C#."
                         gitHub = ""
@@ -97,7 +97,7 @@ class Projects extends Component {
                  <div className="projects-grid">
                 {/*Librari*/}
                 <Project 
-                    projectPicture = "url(https://i.ibb.co/MgZG75Q/Librari.png) center / cover"
+                    projectPicture = "url(https://raw.githubusercontent.com/swedhag/portfolio2/master/public/Librari.png) center / cover"
                     projectTitle = "Librari - an Online Library"
                     projectDescription = "Librari is a web application which my project group and I made in the spring of 2018. Developed with the Google Books API, the intention behind the application was to provide a natural place for users to organize their digital libraries. The application is hosted with Firebase, a Google service which also provided us with user authentication functionalities. Upon registering and logging into the app, users are able to search and find any books in the API and store them on labelled and personalizable book shelves in the MyLibrari page. Nielsen's heuristics were largely considered in the development of the app. The app was coded in JSX and utilizes the React.js framework."
                     gitHub = "https://github.com/swedhag/Librari2"
@@ -109,7 +109,7 @@ class Projects extends Component {
 
                 {/*Portfolio*/ }
                 <Project 
-                    projectPicture = "url(https://i.ibb.co/4drsrGz/portfolio-Pic.png) center / cover"
+                    projectPicture = "url(https://raw.githubusercontent.com/swedhag/portfolio2/master/public/portfolioPic.png) center / cover"
                     projectTitle = "My Portfolio"
                     projectDescription = "I created this portfolio with two key purposes in mind. On one hand, this site should work as a place in which I can track my progress in terms of project work. With 5 years of studies at KTH under my belt, the projects I have undertaken are scattered all over the web. Either in submission forms on the KTH school web, on GitHub or at DiVA, to name a few places. For myself, I want this to be the place in which I can gather the work I am most proud of. A second purpose of this site is to showcase my work and provide examples of my competences. Some would say my program at KTH has a rather broad scope. I hope this broadness is somewhat reflected by this portfolio."
                     gitHub = "https://github.com/swedhag/portfolio2"
